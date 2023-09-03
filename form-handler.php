e889d454e66456170d253fb8d284789ab2f994eb
paramte2 <143928889+paramte2@users.noreply.github.com>
2023-09-03 13:16:23 +0530

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,42 @@
+<?php
+$name= $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject =$_POST['subject'];
+$message =$_POST['message'];
+
+
+$emmail_from = 'paramtelsolution.great-site.in';
+
+$email_subject ='New Form Submission';
+
+$email_body = "User Name: $name.\n". 
+                "User Email: $visitor_email.\n".
+                "Subject: $subject.\n".
+                "User message: $message .\n";
+                
+
+$to = 'hariompandeyg2000@gmail.com';
+
+
+$headers = "From: $email_from \r\n";
+
+
+$header .=  "Reply-To: $visitor_email \r\n";
+
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+
+// Database Connection
+
+/*$conn = new mysqli('localhost','root','','dcu');
+if($conn->connect_error){
+    die('Connection Failed : '.$conn->connect_error);
+}
+else{
+    $stmt = $conn->prepare("insert into registraion(name,email,subject,message)values(?,?,?,?)");
+    $stmt->bind_param()
+}
+*/
+?>
